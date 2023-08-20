4263ec0ed4734e26d1eb022a7c2d63b0c9bd6825
Ryan Lundy <rlundy@gmail.com>
2023-08-20 09:21:33 +0200

    Implement Year 2015 Day 1 Part 2.

--- src/Year2015.cpp
+++ src/Year2015.cpp
@@ -9,5 +9,18 @@ int Year2015::Day1Part1(const std::string& input)
 
 int Year2015::Day1Part2(const std::string &input)
 {
-    return 0;
+    auto floor = 0;
+    for (auto i = 0; i < input.length(); i++) {
+        auto step = input[i];
+        if (step == '(') {
+            floor++;
+        }
+        else if (step == ')') {
+            floor--;
+        }
+        if (floor < 0) {
+            return i + 1;
+        }
+    }
+    return -1;
 }
