c056da1d77ed02f68d2ca93bb33096a74fabacb7
Ryan Lundy <rlundy@gmail.com>
2023-08-21 11:03:51 +0200

    Add Year2016 output on run.

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@ using std::cout;
 using std::endl;
 
 int main() {
-    cout << "Hello, world!" << endl;
-
     Year2015 y;
     auto input2015day1 = readFileContents("C:/Users/rlund/OneDrive/Code/CppAdventOfCode/puzzleInput/Year2015Day1.txt");
     cout << "Year 2015 day 1 part 1: " << y.Day1Part1(input2015day1) << endl;
     cout << "Year 2015 day 1 part 2: " << y.Day1Part2(input2015day1) << endl;
+
+    auto input2016day1 = readFileContents("C:/Users/rlund/OneDrive/Code/CppAdventOfCode/puzzleInput/Year2016Day1.txt");
+    cout << "Year 2016 day 1 part 1: " << y.Day1Part1(input2016day1) << endl;
+    cout << "Year 2016 day 1 part 2: " << y.Day1Part2(input2016day1) << endl;
 }
