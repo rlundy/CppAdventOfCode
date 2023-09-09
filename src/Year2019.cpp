d291eb48d8efb6e253cd04f3d5dc8e786c628d8d
Ryan Lundy <rlundy@gmail.com>
2023-09-09 12:18:56 +0200

    Add framework for year 2019 day 1.

---
+++ src/Year2019.cpp
@@ -0,0 +1,11 @@
+#include "Year2019.hpp"
+
+int Year2019::Day1Part1(const std::string &input)
+{
+    return -1;
+}
+
+int Year2019::Day1Part2(const std::string &input)
+{
+    return -1;
+}
---
+++ tests/Year2019Tests.cpp
@@ -0,0 +1,19 @@
+#include "catch_amalgamated.hpp"
+#include "Year2019.hpp"
+
+TEST_CASE("Year 2019 Day 1 Part 1") {
+    Year2019 y;
+    auto test = std::bind(&Year2019::Day1Part1, &y, std::placeholders::_1);
+    CHECK(test("12") == 2);
+    CHECK(test("14") == 2);
+    CHECK(test("1969") == 654);
+    CHECK(test("100756") == 33583);
+}
+
+TEST_CASE("Year 2019 Day 1 Part 2") {
+    Year2019 y;
+    auto test = std::bind(&Year2019::Day1Part2, &y, std::placeholders::_1);
+    CHECK(test("14") == 2);
+    CHECK(test("1969") == 966);
+    CHECK(test("100756") == 50346);
+}
