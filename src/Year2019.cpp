cbaf3b950b204fd67eaa85f7b95c359adc26466e
Ryan Lundy <rlundy@gmail.com>
2023-09-09 15:15:20 +0200

    Complete Year 2019, Day 1, Part 1.

--- src/Year2019.cpp
+++ src/Year2019.cpp
@@ -1,8 +1,19 @@
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <iterator>
+
 #include "Year2019.hpp"
+#include "Util.hpp"
 
 int Year2019::Day1Part1(const std::string &input)
 {
-    return -1;
+    auto massesText = split(input, "\n");
+    auto masses = textToInt(massesText);
+    std::vector<int> fuels;
+    std::transform(masses.cbegin(), masses.cend(), std::back_inserter(fuels), [](int mass){ return mass / 3 - 2; });
+    return std::accumulate(fuels.cbegin(), fuels.cend(), 0);
 }
 
 int Year2019::Day1Part2(const std::string &input)
