2f81cd879edc6b3e30066992ed32ccf1e8ce82c2
Ryan Lundy <rlundy@gmail.com>
2023-09-10 10:47:59 +0200

    Complete Year 2021 Day 1 Parts 1 and 2.

--- src/Year2021.cpp
+++ src/Year2021.cpp
@@ -1,9 +1,33 @@
 #include "Year2021.hpp"
 
+#include "Util.hpp"
+
 int Year2021::Day1Part1(const std::string& input) {
-    return -1;
+    auto depths = inputToVector(input);
+    auto increases { 0 };
+    for (auto i = 0; i < depths.size() - 1; i++) {
+        auto from { depths[i] };
+        auto to { depths[i + 1] };
+        if (to > from) {
+            increases++;
+        }
+    }
+    return increases;
 }
 
 int Year2021::Day1Part2(const std::string& input) {
-    return -1;
+    auto depths = inputToVector(input);
+    auto increases { 0 };
+    for (auto i = 0; i < depths.size() - 3; i++) {
+        auto a1 { depths[i] };
+        auto a2 { depths[i + 1] };
+        auto a3 { depths[i + 2] };
+        auto a4 { depths[i + 3] };
+        auto first { a1 + a2 + a3 };
+        auto second { a2 + a3 + a4 };
+        if (second > first) {
+            increases++;
+        }
+    }
+    return increases;
 }
