f1df2db83e350d510ae7af4a7883c28c9d7a0717
Ryan Lundy <rlundy@gmail.com>
2023-09-10 09:27:41 +0200

    Finish Year 2019, Day 1, Part 2.

--- include/Year2019.hpp
+++ include/Year2019.hpp
@@ -3,6 +3,9 @@
 #pragma once
 
 class Year2019 {
+    private:
+        int getFuel(const int mass);
+        int getFuelRecursive(const int mass);
     public:
         int Day1Part1(const std::string& input);
         int Day1Part2(const std::string& input);
--- src/Year2019.cpp
+++ src/Year2019.cpp
@@ -7,16 +7,38 @@
 #include "Year2019.hpp"
 #include "Util.hpp"
 
+int Year2019::getFuel(const int mass) {
+    return mass / 3 - 2;
+}
+
+int Year2019::getFuelRecursive(const int mass) {
+    auto currentMass { mass };
+    auto fuel { 0 };
+    while (currentMass > 0) {
+        auto newFuel { currentMass / 3 - 2 };
+        if (newFuel < 0) {
+            newFuel = 0;
+        }
+        fuel += newFuel;
+        currentMass = newFuel;
+    }
+    return fuel;
+}
+
 int Year2019::Day1Part1(const std::string &input)
 {
     auto massesText { split(input, "\n") };
     auto masses { textToInt(massesText) };
     std::vector<int> fuels;
-    std::transform(masses.cbegin(), masses.cend(), std::back_inserter(fuels), [](int mass){ return mass / 3 - 2; });
+    std::transform(masses.cbegin(), masses.cend(), std::back_inserter(fuels), [this](int mass){ return getFuel(mass); });
     return std::accumulate(fuels.cbegin(), fuels.cend(), 0);
 }
 
 int Year2019::Day1Part2(const std::string &input)
 {
-    return -1;
+    auto massesText { split(input, "\n") };
+    auto masses { textToInt(massesText) };
+    std::vector<int> fuels;
+    std::transform(masses.cbegin(), masses.cend(), std::back_inserter(fuels), [this](int mass){ return getFuelRecursive(mass); });
+    return std::accumulate(fuels.cbegin(), fuels.cend(), 0);
 }
