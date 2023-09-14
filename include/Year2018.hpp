3de1eef6da4642faea8252f4f961b91dcff4e4c1
Ryan Lundy <rlundy@gmail.com>
2023-09-14 09:05:12 +0200

    Move free functions to Year2018 class members.

--- include/Year2018.hpp
+++ include/Year2018.hpp
@@ -2,10 +2,17 @@
 
 #include <string>
 
+typedef std::unordered_map<char, int> letterGroup;
+
 class Year2018 {
+    private:
+        auto group(std::string letters);
+        auto hasCount(letterGroup group, int expectedRepeatedLetterCount);
+        auto hasOneThreeLetterSet(letterGroup group);
+        auto hasOneTwoLetterSet(letterGroup group);
     public:
         int Day1Part1(const std::string& input);
         int Day1Part2(const std::string& input);
-        int Day2Part1(const std::string& input);
+        int Day2Part1(const std::string &input);
         std::string Day2Part2(const std::string& input);
 };
