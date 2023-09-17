368229d9d0e047bbd88c082784c7aa363a611f10
Ryan Lundy <rlundy@gmail.com>
2023-09-17 18:28:42 +0200

    Make most Year2016 methods private.

--- include/Year2016.hpp
+++ include/Year2016.hpp
@@ -7,12 +7,13 @@
 #include "KeypadKey.hpp"
 
 class Year2016 {
-    public:
-        CompassDirection makeTurn(const CompassDirection direction, const char turn);
+    private:
         std::pair<int, int> getNextMove(CompassDirection direction);
         auto getNineKeyPad();
         auto getThirteenKeyPad();
         auto getCode(std::map<char, KeypadKey> keys, const std::string &input);
+    public:
+        CompassDirection makeTurn(const CompassDirection direction, const char turn);
         int Day1Part1(const std::string &input);
         int Day1Part2(const std::string& input);
         std::string Day2Part1(const std::string& input);
