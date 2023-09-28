63260073925bc20e85301268fda3427b242fc5de
Ryan Lundy <rlundy@gmail.com>
2023-09-28 19:29:09 +0200

    Remove unused constructor.

--- include/Rectangle.hpp
+++ include/Rectangle.hpp
@@ -3,7 +3,6 @@
 class Rectangle {
     private:
     public:
-        Rectangle(int x, int y, int width, int height);
         Rectangle(const std::string& input);
         int x;
         int y;
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,8 +1,6 @@
 #include "Rectangle.hpp"
 #include "Util.hpp"
 
-Rectangle::Rectangle(int x, int y, int width, int height) : x { x }, y { y }, width { width }, height { height } { }
-
 Rectangle::Rectangle(const std::string &line)
 {
     auto parts { split(line, " ") };
