9cbda17c5c719714f6a4b8e5a7b4754ce0d9df1f
Ryan Lundy <rlundy@gmail.com>
2023-09-28 20:00:29 +0200

    Clean up Rectangle constructor with use of istringstream.

--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,14 +1,11 @@
+#include <sstream>
+
 #include "Rectangle.hpp"
 #include "Util.hpp"
 
 Rectangle::Rectangle(const std::string &line)
 {
-    auto parts { split(line, " ") };
-    auto xyText { split(parts[2], ",") };
-    x = std::stoi(xyText[0]);
-    y = std::stoi(xyText[1]);
-    auto whText { split(parts[3], "x") };
-    width = std::stoi(whText[0]);
-    height = std::stoi(whText[1]);
-    id = std::stoi(parts[0].substr(1));
+    std::istringstream iss(line);
+    char hash, at, comma, colon, by;
+    iss >> hash >> id >> at >> x >> comma >> y >> colon >> width >> by >> height;
 }
